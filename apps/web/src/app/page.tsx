e65f675837977755e725e4a9e9841891b81624b0
apsinghdev <ajeetpratap517@gmail.com>
2024-11-13 23:17:24 +0530

    chore: install and configure shadcn

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,15 @@
+'use client';
+
 export default function Home() {
   return (
-    <>
-    </>
+    <div>
+      <div>
+        <h1>Opensox</h1>
+        <h1>Find perfect open-source project for yourself</h1>
+        <div id="landing-footer">
+          <h1>Created by <button>Ajeet</button></h1>
+        </div>
+      </div>
+    </div>
   );
 }
