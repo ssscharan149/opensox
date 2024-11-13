eabb2423455d5c45d68b34cc7fff0b07d185c336
apsinghdev <ajeetpratap517@gmail.com>
2024-11-14 01:17:27 +0530

    feat: create ui of landing page (mobile)

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h1>Opensox</h1>
-        <h1>Find perfect open-source project for yourself</h1>
-        <div id="landing-footer">
-          <h1>Created by <button>Ajeet</button></h1>
+    <div className="flex flex-col h-screen w-full justify-center items-center">
+      <div className="flex flex-col items-center justify-center w-[90%] h-[60%] space-y-20">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <h1 className="text-5xl font-extrabold">Opensox</h1>
+          <h1 className="text-sm font-medium text-theme-color">Find perfect open-source project for yourself</h1>
         </div>
+        <Button className="font-bold">Get Started</Button>
+      </div>
+      <div id="landing-footer" className="absolute bottom-7">
+        <h1 className="text-sm">Created by <button className="underline text-theme-color">Ajeet</button></h1>
       </div>
     </div>
   );
