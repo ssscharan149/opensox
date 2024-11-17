a4ea3e8092b8ba6becb4dab0f5d4a406ed798fb0
apsinghdev <ajeetpratap517@gmail.com>
2024-11-17 21:57:55 +0530

    feat: create icon wrapper

---
+++ apps/web/src/components/ui/IconWrapper.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import React from "react";
+
+interface IconWrapperProps extends React.HTMLAttributes<HTMLDivElement> {
+    children: React.ReactNode;
+}
+
+export const IconWrapper = ({ children, className, ...props }: IconWrapperProps) => {
+    return (
+        <div
+            className={cn(
+                "p-0.5 rounded-sm border border-border-color hover:bg-gray-100 cursor-pointer",
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </div>
+    )
+}
