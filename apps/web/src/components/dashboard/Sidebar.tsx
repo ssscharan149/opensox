5fc9c476b1f525dbd8d0eb9ddddfc81b41d470b4
apsinghdev <ajeetpratap517@gmail.com>
2024-12-04 00:03:29 +0530

    feat/client: highlight the current sidebar route

--- apps/web/src/components/dashboard/Sidebar.tsx
+++ apps/web/src/components/dashboard/Sidebar.tsx
@@ -1,16 +1,32 @@
-"use client"
-// import { XMarkIcon } from "@heroicons/react/24/outline"
-// import { IconWrapper } from "../ui/IconWrapper"
-import SidebarItem from "../sidebar/SidebarItem"
+"use client";
+import Link from "next/link";
+import SidebarItem from "../sidebar/SidebarItem";
+import { usePathname } from "next/navigation";
+
+const SIDEBAR_ROUTES = [
+  {
+    path: "/dashboard/home",
+    label: "Home",
+  },
+  {
+    path: "/dashboard/projects",
+    label: "Projects",
+  },
+];
+
+const getSidebarLinkClassName = (currentPath: string, routePath: string) => {
+  const isActive = currentPath === routePath;
+  return `${isActive ? "text-ox-purple" : "text-ox-white"}`;
+};
 
 export default function Sidebar() {
+  const pathname = usePathname();
 
-    const aboutAjClickHandler = () => {
-    }
+  const aboutAjClickHandler = () => {};
 
-    return (
-        <div className="h-full z-50 flex flex-col rounded-r-lg bg-ox-black-1">
-            {/* <div className="sidebar-header flex justify-between px-2 py-3 border-b border-ox-gray">
+  return (
+    <div className="h-full z-50 flex flex-col rounded-r-lg bg-ox-black-1">
+      {/* <div className="sidebar-header flex justify-between px-2 py-3 border-b border-ox-gray">
                 <div className="flex items-center space-x-3">
                     <h1 className="text-md font-semibold text-ox-purple">Opensox</h1>
                 </div>
@@ -21,13 +37,31 @@ export default function Sidebar() {
                 </div>
             </div> */}
 
-            <div className="sidebar-body flex-grow flex-col overflow-y-auto px-2 py-4">
-                <SidebarItem itemName="Home" onclick={aboutAjClickHandler}></SidebarItem>
-                <SidebarItem itemName="Projects" onclick={aboutAjClickHandler}></SidebarItem>
-                <SidebarItem itemName="Request a feature" onclick={aboutAjClickHandler}></SidebarItem>
-                <SidebarItem itemName="Support the project" onclick={aboutAjClickHandler}></SidebarItem>
-                <SidebarItem itemName="About Ajeet" onclick={aboutAjClickHandler}></SidebarItem>
-            </div>
-        </div>
-    )
+      <div className="sidebar-body flex-grow flex-col overflow-y-auto px-2 py-4">
+        {SIDEBAR_ROUTES.map((route) => {
+          return (
+            <Link
+              href={route.path}
+              className={getSidebarLinkClassName(pathname, route.path)}
+              key={route.path}
+            >
+              <SidebarItem itemName={route.label}></SidebarItem>
+            </Link>
+          );
+        })}
+        <SidebarItem
+          itemName="Request a feature"
+          onclick={aboutAjClickHandler}
+        ></SidebarItem>
+        <SidebarItem
+          itemName="Support the project"
+          onclick={aboutAjClickHandler}
+        ></SidebarItem>
+        <SidebarItem
+          itemName="About Ajeet"
+          onclick={aboutAjClickHandler}
+        ></SidebarItem>
+      </div>
+    </div>
+  );
 }
--- apps/web/src/components/sidebar/SidebarItem.tsx
+++ apps/web/src/components/sidebar/SidebarItem.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-export default function SidebarItem({itemName, onclick}: {itemName: string, onclick: () => void}) {
+export default function SidebarItem({itemName, onclick}: {itemName: string, onclick?: () => void}) {
     return (
         <div className="w-full h-[6vh] flex items-center rounded-lg cursor-pointer" onClick={onclick}>
             <h1 className="text-xs font-semibold hover:text-ox-purple pl-6">{itemName}</h1>
