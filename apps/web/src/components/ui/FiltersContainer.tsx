787552cf3668d9474eddc20e96c723b9a3fd9096
apsinghdev <ajeetpratap517@gmail.com>
2024-12-04 23:30:26 +0530

    feat/client: show filtersComponent and format

--- apps/web/src/components/ui/FiltersContainer.tsx
+++ apps/web/src/components/ui/FiltersContainer.tsx
@@ -5,45 +5,68 @@ import { XMarkIcon } from "@heroicons/react/24/outline";
 import { IconWrapper } from "./IconWrapper";
 import { Accordion } from "./accordion";
 import Filter from "./Filter";
+import { useFilterStore } from "@/store/useFilterStore";
 
 export default function FiltersContainer() {
-    return (
-        <div className="w-[90%] h-[70vh] flex flex-col rounded-lg border border-ox-gray relative bg-ox-black-1">
-            <div
-                id="filter-header"
-                className="flex w-full h-[10%] border-b border-ox-gray justify-between items-center px-2"
-            >
-                <h1 className="text-sm text-ox-purple font-semibold">Filters</h1>
-                <IconWrapper>
-                    <XMarkIcon className="size-4 text-ox-purple"></XMarkIcon>
-                </IconWrapper>
-            </div>
+  const { toggleShowFilters } = useFilterStore();
+  return (
+    <div className="w-80 h-96 z-10 flex flex-col rounded-lg border border-ox-gray absolute bg-ox-black-1 right-20">
+      <div
+        id="filter-header"
+        className="flex w-full h-[10%] border-b border-ox-gray justify-between items-center px-2"
+      >
+        <h1 className="text-sm text-ox-purple font-semibold">Filters</h1>
+        <IconWrapper>
+          <XMarkIcon
+            className="size-4 text-ox-purple"
+            onClick={() => toggleShowFilters()}
+          ></XMarkIcon>
+        </IconWrapper>
+      </div>
 
-            <div className="flex-1 overflow-y-auto">
-                <Accordion type="multiple" className="w-full">
-                    <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
-                    <Filter filterName="Competitiveness" filters={["high", "low"]}></Filter>
-                    <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
-                    <Filter filterName="Competitiveness" filters={["high", "low"]}></Filter>
-                    <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
-                    <Filter filterName="Competitiveness" filters={["high", "low"]}></Filter>
-                    <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
-                    <Filter filterName="Competitiveness" filters={["high", "low"]}></Filter>
-                    <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
-                    <Filter filterName="Competitiveness" filters={["high", "low"]}></Filter>
-                    <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
-                    <Filter filterName="Competitiveness" filters={["high", "low"]}></Filter>
-                </Accordion>
-            </div>
+      <div className="flex-1 overflow-y-auto">
+        <Accordion type="multiple" className="w-full">
+          <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
+          <Filter
+            filterName="Competitiveness"
+            filters={["high", "low"]}
+          ></Filter>
+          <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
+          <Filter
+            filterName="Competitiveness"
+            filters={["high", "low"]}
+          ></Filter>
+          <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
+          <Filter
+            filterName="Competitiveness"
+            filters={["high", "low"]}
+          ></Filter>
+          <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
+          <Filter
+            filterName="Competitiveness"
+            filters={["high", "low"]}
+          ></Filter>
+          <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
+          <Filter
+            filterName="Competitiveness"
+            filters={["high", "low"]}
+          ></Filter>
+          <Filter filterName="Tech stack" filters={["py", "ts", "js"]}></Filter>
+          <Filter
+            filterName="Competitiveness"
+            filters={["high", "low"]}
+          ></Filter>
+        </Accordion>
+      </div>
 
-            <div
-                id="filter-footer"
-                className="w-full h-[10%] border-t border-ox-gray flex items-center px-2 justify-end"
-            >
-                <Button className="font-semibold text-white bg-ox-purple text-[14px] h-6 w-12 hover:bg-white-500">
-                    Search
-                </Button>
-            </div>
-        </div>
-    );
+      <div
+        id="filter-footer"
+        className="w-full h-[10%] border-t border-ox-gray flex items-center px-2 justify-end"
+      >
+        <Button className="font-semibold text-white bg-ox-purple text-[14px] h-6 w-12 hover:bg-white-500">
+          Search
+        </Button>
+      </div>
+    </div>
+  );
 }
