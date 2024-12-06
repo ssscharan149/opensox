5690b88b005d97bc1ba00feff17c0f20cbe5a8c8
apsinghdev <ajeetpratap517@gmail.com>
2024-12-07 00:27:45 +0530

    feat/client: make wip filters distinguish from other filters

--- apps/web/src/components/ui/Filter.tsx
+++ apps/web/src/components/ui/Filter.tsx
@@ -4,6 +4,7 @@ import { AccordionContent, AccordionItem, AccordionTrigger } from "./accordion";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { useFilterInputStore } from "@/store/useFilterInputStore";
+import clsx from "clsx";
 
 export default function Filter({
   filterName,
@@ -20,13 +21,28 @@ export default function Filter({
     inputData[filterName] = filter;
     updateFilters(inputData);
   };
+
+  const triggerClasses = clsx("text-sm font-medium", {
+    "text-slate-500": ["Hire contributors", "Funding", "Trending"].includes(
+      filterName
+    ),
+  });
+
   return (
     <div onClick={onClick}>
       <AccordionItem value={filterName} className="px-3">
-        <AccordionTrigger className="text-sm font-medium">
+        <AccordionTrigger className={triggerClasses}>
           {filterName}
         </AccordionTrigger>
-        <AccordionContent>
+        <AccordionContent
+          className={
+            filterName === "Hire contributors" ||
+            filterName === "Funding" ||
+            filterName === "Trending"
+              ? "text-white-100"
+              : ""
+          }
+        >
           <RadioGroup>
             {filters.map((filter) => (
               <div key={filter} className="flex items-center space-x-2">
