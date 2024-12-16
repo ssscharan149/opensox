dfcb0ece0071258cb94d16594f4b1c08e0c1d719
apsinghdev <ajeetpratap517@gmail.com>
2024-12-16 20:57:28 +0530

    feat/server: create fn to increase the tota query count by one when a query is made

--- apps/backend/src/routes/projects.route.ts
+++ apps/backend/src/routes/projects.route.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 import { fetchRepositories } from "../services/project.service.ts";
+import { increaseQueriesCount } from "src/utils/queryCount.ts";
 
 const projectsRouter = express.Router();
 
 projectsRouter.post("/get_projects", async (req: Request, res: Response) => {
+  increaseQueriesCount()
   const filters = req.body;
   const repos = await fetchRepositories(filters);
   res.json(repos);
---
+++ apps/backend/src/utils/queryCount.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const increaseQueriesCount = async (): Promise<void> => {
+  try {
+    const updatedCount = await prisma.queryCount.update({
+      where: { id: 1 },
+      data: {
+        total_queries: {
+          increment: 1,
+        },
+      },
+    });
+    console.log("Updated Count:", updatedCount);
+  } catch (error) {
+    console.error("Error updating query count:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
