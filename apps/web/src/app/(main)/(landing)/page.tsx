61b13600e1f3fb2acb485266fbca212e17d0dc5d
apsinghdev <ajeetpratap517@gmail.com>
2024-12-16 14:25:25 +0530

    feat/client: redirect user to dashboard

--- apps/web/src/app/(main)/(landing)/page.tsx
+++ apps/web/src/app/(main)/(landing)/page.tsx
@@ -4,25 +4,39 @@ import Image from "next/image";
 import imgSrc from "../../../assets/images/demoss.png";
 import { GlowingBtn } from "@/components/ui/GlowingBtn";
 import NumberTicker from "@/components/ui/number-ticker";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   const ajeetClickHandler = (): void => {
     window.open("https://x.com/ajeetunc", "_blank");
+  };
+
+  const getStartedHandler = (): void => {
+    router.push("/dashboard");
   };
 
   return (
     <div className="h-screen w-full overflow-y-auto scroll-smooth">
       <section className="h-screen flex flex-col justify-center items-center text-center px-4 mb-12">
         <div className="space-y-8 max-w-2xl">
-          <h1 className="text-5xl lg:text-7xl font-extrabold font-sans">Opensox</h1>
+          <h1 className="text-5xl lg:text-7xl font-extrabold font-sans">
+            Opensox
+          </h1>
           <p className="text-xs lg:text-base lg:font-medium font-medium text-ox-purple">
             Find the perfect open-source project to contribute.
           </p>
-          <GlowingBtn text="Get started"></GlowingBtn>
+          <GlowingBtn
+            text="Get started"
+            handleClick={getStartedHandler}
+          ></GlowingBtn>
         </div>
       </section>
       <section className="h-screen flex flex-col items-center justify-center px-4 relative gap-28">
-      <h1 className="text-5xl lg:text-7xl font-extrabold font-sans">Save your 100+ hours</h1>
+        <h1 className="text-5xl lg:text-7xl font-extrabold font-sans">
+          Save your 100+ hours
+        </h1>
         <div
           className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
               bg-[#9455f4] opacity-60 blur-[250px] 
@@ -40,7 +54,9 @@ export default function Home() {
         </div>
       </section>
       <section className="h-screen flex flex-col justify-center items-center text-center px-4 gap-28">
-        <h1 className="text-5xl lg:text-7xl font-extrabold font-sans">Queries fetched so far</h1>
+        <h1 className="text-5xl lg:text-7xl font-extrabold font-sans">
+          Queries fetched so far
+        </h1>
         <p className="whitespace-pre-wrap text-9xl font-semibold font-sans tracking-tighter text-black dark:text-white bg-gradient-to-r from-[#9455f4] to-[#9455f4]/50 bg-clip-text">
           <NumberTicker value={1000} />
         </p>
