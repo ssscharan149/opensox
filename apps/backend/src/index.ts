2e8bb6bab7d2c238f2fe6e5bcb43d992241f29c4
apsinghdev <ajeetpratap517@gmail.com>
2024-12-16 15:46:42 +0530

    chore: log confirmation msg when server is connected with db

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import apiRouter from "./api_routes.ts";
 import cors from "cors";
+import prisma from "./prisma.ts";
 
 const App: Express = express();
 const PORT = 8080;
@@ -20,5 +21,16 @@ App.use("/api", apiRouter);
 App.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+async function connectToDatabase() {
+  try {
+    await prisma.$connect();
+    console.log("Successfully connected to the database");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+}
+
+connectToDatabase();
 
 export { App };
