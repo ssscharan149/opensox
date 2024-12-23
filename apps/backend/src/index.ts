36df3a779b23fe8aa4f9aafb3e4659bb81595b3b
apsinghdev <ajeetpratap517@gmail.com>
2024-12-23 14:23:42 +0530

    fix/cors: allow multiple origins to hit api

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -5,11 +5,25 @@ import prisma from "./prisma";
 
 const App: Express = express();
 const PORT = process.env.PORT || 8080;
-const CORS_ORIGIN = (process.env.CORS_ORIGIN || "http://localhost:3000").split(
-  ","
-);
-const corsOptions = {
-  origin: CORS_ORIGIN,
+const CORS_ORIGINS = process.env.CORS_ORIGINS
+  ? process.env.CORS_ORIGINS.split(",")
+  : ["http://localhost:3000", "http://localhost:5000"];
+
+interface CorsOptions {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, origin?: string) => void
+  ) => void;
+}
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || CORS_ORIGINS.includes(origin)) {
+      callback(null, origin);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
 
 App.use(cors(corsOptions));
