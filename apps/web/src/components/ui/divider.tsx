f8ea330bb9a4e787f14f7f37018222073c33df0b
apsinghdev <ajeetpratap517@gmail.com>
2025-08-16 15:23:16 +0530

    feat: add login ui

---
+++ apps/web/src/components/ui/divider.tsx
@@ -0,0 +1,10 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+const Divider = ({ className }: { className?: string }) => {
+  return (
+    <div className={cn('w-full h-px bg-[#202020]', className)}></div>
+  )
+}
+
+export default Divider
